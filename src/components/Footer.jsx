8d63cc5ce8e270c7d4e5f79a0f43ccf42fc5ace6
Anurag Srivastava <anurag.as.srivastava@gmail.com>
2020-08-09 01:24:26 +0530

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,50 @@
 import React from "react";
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
+import PauseCircleOutlineIcon from "@material-ui/icons/PauseCircleOutline";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
 import "./Footer.css";
+import { Grid, Slider } from "@material-ui/core";
 const Footer = () => {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <div className="footer__left">
+        <img
+          className="footer__albumLogo"
+          src="https://upload.wikimedia.org/wikipedia/en/6/62/Usher8701Cover.jpg"
+          alt=""
+        />
+        <div className="footer__songInfo">
+          <h4>Yeah!</h4>
+          <p>Usher</p>
+        </div>
+      </div>
+      <div className="footer__center">
+        <ShuffleIcon className="footer__green"></ShuffleIcon>
+        <SkipPreviousIcon className="footer__icon"></SkipPreviousIcon>
+        <PauseCircleOutlineIcon fontSize="large"></PauseCircleOutlineIcon>
+        <SkipNextIcon className="footer__icon"></SkipNextIcon>
+        <RepeatIcon className="footer__green"></RepeatIcon>
+      </div>
+      <div className="footer__right">
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+          </Grid>
+          <Grid item xs>
+            <Slider aria-labelledby="continuous-slider" />
+          </Grid>
+        </Grid>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
